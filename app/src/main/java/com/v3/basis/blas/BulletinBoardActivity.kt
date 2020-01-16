6d4d4a9f920cc519fa197808f6d22e60f697780a
kijim <k-kijima@basis-corp.jp>
2020-01-16 17:18:01 +0900

    first commit

---
+++ app/src/main/java/com/v3/basis/blas/BulletinBoardActivity.kt
@@ -0,0 +1,16 @@
+package com.v3.basis.blas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.preference.PreferenceManager
+
+class BulletinBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bulletin_board)
+    }
+
+}
---
+++ app/src/main/java/com/v3/basis/blas/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.v3.basis.blas
+
+import android.content.Intent
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceManager
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
