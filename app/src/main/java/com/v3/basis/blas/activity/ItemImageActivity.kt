6172ea5112f7e417c3d93d845b0b0c95e1b639f3
fukuda <fukuda.work2020@gmail.com>
2020-03-18 18:36:52 +0900

    Update fragment, activity

--- app/src/main/java/com/v3/basis/blas/activity/ItemImageActivity.kt
+++ app/src/main/java/com/v3/basis/blas/activity/ItemImageActivity.kt
@@ -1,16 +1,53 @@
 package com.v3.basis.blas.activity
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.fragment.app.FragmentTransaction
 import com.v3.basis.blas.R
 import com.v3.basis.blas.ui.ext.showBackKeyForActionBar
+import com.v3.basis.blas.ui.item.item_image.ItemImageFragment
+import com.v3.basis.blas.ui.terminal.TerminalFragment
 
 class ItemImageActivity : AppCompatActivity() {
+
+    companion object {
+        const val TOKEN = "token"
+        const val PROJECT_ID = "project_id"
+        const val ITEM_ID = "item_id"
+
+        fun createIntent(context: Context, token: String?, projectId: String?, itemId: String?) : Intent {
+
+            return Intent(context, ItemImageActivity::class.java).apply {
+                putExtra(TOKEN, token)
+                putExtra(PROJECT_ID, projectId)
+                putExtra(ITEM_ID, itemId)
+            }
+        }
+    }
+
+    private val token:String?
+        get() = intent.extras?.getString(TOKEN)
+
+    private val projectId: String?
+        get() = intent.extras?.getString(PROJECT_ID)
+
+    private val itemId: String?
+        get() = intent.extras?.getString(ITEM_ID)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_image)
+
+        if (savedInstanceState == null) {
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            val fragment = ItemImageFragment.newInstance(token, itemId)
+            transaction.replace(R.id.container, fragment)
+            transaction.commit()
+        }
 
         //左上の戻るボタンの処理
         showBackKeyForActionBar()
--- app/src/main/java/com/v3/basis/blas/ui/item/item_image/ItemImageFragment.kt
+++ app/src/main/java/com/v3/basis/blas/ui/item/item_image/ItemImageFragment.kt
@@ -2,25 +2,73 @@ package com.v3.basis.blas.ui.item.item_image
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.v3.basis.blas.R
+import com.v3.basis.blas.blasclass.rest.BlasRestImage
+import com.v3.basis.blas.ui.item.item_image.adapter.ImageAdapterItem
+import com.v3.basis.blas.ui.logout.LogoutViewModel
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.databinding.GroupieViewHolder
+import kotlinx.android.synthetic.main.fragment_item_image.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ItemImageFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    companion object {
+        const val TOKEN = "token"
+        const val ITEM_ID = "item_id"
+
+        fun newInstance(token: String?, itemId: String?) : Fragment {
+            val f = ItemImageFragment()
+            f.arguments = Bundle().apply {
+                putString(TOKEN, token)
+                putString(ITEM_ID, itemId)
+            }
+            return f
+        }
+    }
+
+    private val token:String?
+        get() = arguments?.getString(TOKEN)
+
+    private val itemId: String?
+        get() = arguments?.getString(ITEM_ID)
+
+    private lateinit var viewModel: ItemImageViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.fragment_item_image, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        viewModel = ViewModelProviders.of(this).get(ItemImageViewModel::class.java)
+        viewModel.fetchSuccess.observe(this, Observer {
+            val items = it.records?.map { records ->
+                ImageAdapterItem(viewModel, records.Image.mapToItemImageCellItem())
+            }
+            items?.also { hash ->
+                val gAdapter = GroupAdapter<GroupieViewHolder<*>>()
+                recyclerView.adapter = gAdapter
+                gAdapter.update(hash.toList())
+            }
+        })
+        viewModel.fetchError.observe(this, Observer {
+
+        })
+
+        viewModel.fetch(token, itemId)
+
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+    }
 }
