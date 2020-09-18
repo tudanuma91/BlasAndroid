6a0f288501cc26e927324f921ee8b1c3cf26a6c2
tudanuma91 <miyokawa@tudanuma.sakura.ne.jp>
2020-09-18 18:50:13 +0900

    base64エンコードして送信できるはずだ(テストまだ！)

--- app/src/main/java/com/v3/basis/blas/blasclass/rest/SyncBlasRestInquiry.kt
+++ app/src/main/java/com/v3/basis/blas/blasclass/rest/SyncBlasRestInquiry.kt
@@ -1,9 +1,12 @@
 package com.v3.basis.blas.blasclass.rest
 
 import android.content.Context
+import android.util.Base64
 import android.util.Log
 import com.v3.basis.blas.blasclass.app.BlasApp
+import com.v3.basis.blas.blasclass.app.BlasApp.Companion.token
 import com.v3.basis.blas.blasclass.worker.DownloadWorker
+import org.json.JSONObject
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileInputStream
@@ -11,28 +14,31 @@ import java.io.FileOutputStream
 import java.nio.charset.Charset
 import java.nio.file.Paths
 import java.util.*
+
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
 class SyncBlasRestInquiry : SyncBlasRest() {
 
+    val zipDir =  BlasApp.applicationContext().dataDir.path + "/temp/"
 
-    fun execute() {
-        Log.d("SyncBlasRestInquiry.execute()","start")
+    private fun create_zip( zipFile:String ) {
 
         val context = BlasApp.applicationContext()
         val preferences = context.getSharedPreferences(DownloadWorker.COMPLETED_DOWNLOAD, Context.MODE_PRIVATE)
         val all = preferences.all
 
-        val zipDir =  BlasApp.applicationContext().dataDir.path + "/temp/"
+//        val zipDir =  BlasApp.applicationContext().dataDir.path + "/temp/"
         val directory = File(zipDir)
         directory.mkdirs()
 
-        val zipFile = UUID.randomUUID().toString() + ".zip"
+//        val zipFile = UUID.randomUUID().toString() + ".zip"
         val outputPath = Paths.get( zipDir + zipFile )
         val zipFileCoding: Charset = Charset.forName("Shift_JIS")
         val zipBufferSize = 1024 * 1024
 
+        // TODO:ここでやるべきかは？？？
+        // zipを作る
         FileOutputStream( outputPath.toString()  ).use {fileOutputStream ->
             ZipOutputStream( BufferedOutputStream(fileOutputStream), zipFileCoding).use { zipOutStream ->
                 all.forEach { key, filePath ->
@@ -48,6 +54,68 @@ class SyncBlasRestInquiry : SyncBlasRest() {
                     zipOutStream.closeEntry()
                 }
             }
+        }
+
+    }
+
+    private fun createBase64( file:File ) : String? {
+
+        var base64 : String? = null
+        try {
+            val fis = FileInputStream(file)
+            val length = file.length()
+
+            val bytes = ByteArray(file.length().toInt())
+            fis.read(bytes)
+            base64 = Base64.encodeToString(bytes,Base64.DEFAULT)
+        }
+        catch ( ex : Exception ) {
+            throw ex
+        }
+
+        return base64
+    }
+
+    fun execute() {
+        Log.d("SyncBlasRestInquiry.execute()","start")
+
+        // zipファイル生成
+        val zipFile = UUID.randomUUID().toString() + ".zip"
+        create_zip( zipFile )
+
+        // base64化
+        val file = File( zipDir + zipFile )
+        val base64Zip = createBase64(file)
+
+        // 送信
+        val payload = mapOf(
+            "token" to token,
+            "zip" to base64Zip
+        )
+
+        var json:JSONObject? = null
+        val method = "POST"
+        val blasUrl = BlasRest.URL + "inquiry/upload/"
+
+        try {
+            val response = super.getResponseData(payload,method, blasUrl)
+            json = JSONObject(response)
+
+        }
+        catch(e: Exception) {
+            Log.d("blas-log", "通信エラー")
+        }
+
+        if (json != null) {
+            val errorCode = json.getInt("error_code")
+
+            if (errorCode == 0) {
+                Log.d("Inquiry send","成功")
+            }
+            else {
+                Log.d("Inquiry send","error!!!!!!!!!!!!!!")
+            }
+
         }
 
         Log.d("SyncBlasRestInquiry.execute()","end")
